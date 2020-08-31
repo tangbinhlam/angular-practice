cabec383631ed883a55860ab2873add2ffc7399f
osalam <tangbinhlam@gmail.com>
2020-08-31 15:27:44 -0500

    [UserProfile] add form to manage profile by using FromBuilder and FromGroup of ReactiveForm

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
 
 import { UserProfile } from '../core/models/user-profile.model';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -12,12 +14,33 @@ import { UserProfile } from '../core/models/user-profile.model';
 })
 export class ProfileComponent implements OnInit {
   userProfile$: Observable<UserProfile>;
-  constructor(public afAuth: AngularFireAuth, public afs: AngularFirestore) {}
+  profileForm: FormGroup = this.fb.group({
+    uid: [''],
+    name: [''],
+    email: [''],
+    address: [''],
+    city: [''],
+    state: [''],
+    zip: [''],
+    phone: [''],
+    specialty: [''],
+    ip: [''],
+  });
+
+  constructor(
+    public afs: AngularFirestore,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+  ) {}
 
   ngOnInit() {
-    this.userProfile$ = this.afAuth.user.pipe(
-      map((user) => this.afs.doc<UserProfile>(`users/${user.uid}`)),
+    this.userProfile$ = this.route.params.pipe(
+      map((param) => param['id']),
+      map((uid) => this.afs.doc<UserProfile>(`users/${uid}`)),
       switchMap((doc) => doc.valueChanges()),
+      tap((userProfile) => this.profileForm.patchValue(userProfile)),
     );
   }
+
+  onUpdate() {}
 }
