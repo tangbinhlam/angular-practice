fe83e21c4f4de540cf2379523590049336c196bb
osalam <tangbinhlam@gmail.com>
2020-08-31 14:58:18 -0500

    [AngularFirestore] display user Profile form collection real data using AngularFirestore

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { User } from 'firebase';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
+import { UserProfile } from '../core/models/user-profile.model';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.sass'],
 })
-export class ProfileComponent {
-  constructor(public afAuth: AngularFireAuth) {}
+export class ProfileComponent implements OnInit {
+  userProfile$: Observable<UserProfile>;
+  constructor(public afAuth: AngularFireAuth, public afs: AngularFirestore) {}
+
+  ngOnInit() {
+    this.userProfile$ = this.afAuth.user.pipe(
+      map((user) => this.afs.doc<UserProfile>(`users/${user.uid}`)),
+      switchMap((doc) => doc.valueChanges()),
+    );
+  }
 }
