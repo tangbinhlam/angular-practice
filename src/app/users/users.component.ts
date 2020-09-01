ba50edee48cb3809ba901353acecff365a40443e
osalam <tangbinhlam@gmail.com>
2020-08-31 23:51:54 -0500

    [Custom-claims] modify rule of firestore and storage allow admin see the user collection

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+import { UserProfile } from '../core/models/user-profile.model';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.sass']
+  styleUrls: ['./users.component.sass'],
 })
 export class UsersComponent implements OnInit {
+  users$: Observable<UserProfile[]>;
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {}
 
   ngOnInit(): void {
+    this.users$ = this.afs.collection<UserProfile>('users').valueChanges();
   }
-
 }
